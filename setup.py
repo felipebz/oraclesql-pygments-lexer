372776f21613b34a38e85d9155583336bc846ec5
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2012-12-26 21:50:25 -0200

    Added initial Python packaging boilerplate

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = "oraclesql-pygments-lexer",
+    version = "0.1",
+    packages = ['oraclesql',],
+    install_requires = ['Pygments'],
+    author = "Felipe Zorzo",
+    author_email = "felipe.b.zorzo@gmail.com",
+    description = "Pygments lexer for Oracle PL/SQL and Oracle Forms",
+)
